2334d5eef3cd9b3cca99b395525ed8ca44603759
Fernando-Gouvea <fernando.gouvea.lfg@gmail.com>
2020-11-23 12:22:42 -0300

    order status

--- src/main/java/com/demoweb/demo/config/TestConfig.java
+++ src/main/java/com/demoweb/demo/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.demoweb.demo.entities.Order;
 import com.demoweb.demo.entities.User;
+import com.demoweb.demo.entities.enums.OrderStatus;
 import com.demoweb.demo.repositories.OrderRepository;
 import com.demoweb.demo.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "16997503293", "112233445");
 		User u2 = new User(null, "Alex Green", "Alex@gmail.com", "16981121964", "998877665");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),u1);
-		Order o2 = new Order(null, Instant.parse("2018-10-23T22:07:33Z"),u2);
-		Order o3 = new Order(null, Instant.parse("2016-12-26T08:44:10Z"),u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID ,u1);
+		Order o2 = new Order(null, Instant.parse("2018-10-23T22:07:33Z"),OrderStatus.SHIPPED,u2);
+		Order o3 = new Order(null, Instant.parse("2016-12-26T08:44:10Z"),OrderStatus.WAITING_PAYMENT,u1);
 		
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
