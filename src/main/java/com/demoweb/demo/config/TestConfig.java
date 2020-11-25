ee2df79a13d7348ef21124ed9f28afd6a7d514b6
Fernando-Gouvea <fernando.gouvea.lfg@gmail.com>
2020-11-25 15:57:04 -0300

    resourse category

--- src/main/java/com/demoweb/demo/config/TestConfig.java
+++ src/main/java/com/demoweb/demo/config/TestConfig.java
@@ -1,6 +1,7 @@
 package com.demoweb.demo.config;
 
 import java.time.Instant;
+
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.demoweb.demo.entities.Category;
 import com.demoweb.demo.entities.Order;
 import com.demoweb.demo.entities.User;
 import com.demoweb.demo.entities.enums.OrderStatus;
+import com.demoweb.demo.repositories.CategoryRepository;
 import com.demoweb.demo.repositories.OrderRepository;
 import com.demoweb.demo.repositories.UserRepository;
 
@@ -24,16 +27,25 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private OrderRepository orderRepository;
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+	    
+	    	Category cat1 =  new Category(null, "Eletronics");
+	    	Category cat2 =  new Category(null, "Books");
+	    	Category cat3 =  new Category(null, "Computers");
+	    	
+	    	categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+	    		    
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "16997503293", "112233445");
 		User u2 = new User(null, "Alex Green", "Alex@gmail.com", "16981121964", "998877665");
 		
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID ,u1);
 		Order o2 = new Order(null, Instant.parse("2018-10-23T22:07:33Z"),OrderStatus.SHIPPED,u2);
 		Order o3 = new Order(null, Instant.parse("2016-12-26T08:44:10Z"),OrderStatus.WAITING_PAYMENT,u1);
-		
-		
+				
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
---
+++ src/main/java/com/demoweb/demo/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.demoweb.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.demoweb.demo.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
