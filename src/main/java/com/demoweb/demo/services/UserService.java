4d5258e1a281da511a9ac9f492a89eee5792b9eb
Fernando-Gouvea <fernando.gouvea.lfg@gmail.com>
2020-12-08 19:01:33 -0300

    delete exception

--- src/main/java/com/demoweb/demo/services/UserService.java
+++ src/main/java/com/demoweb/demo/services/UserService.java
@@ -1,13 +1,17 @@
 package com.demoweb.demo.services;
 
 import java.util.List;
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.demoweb.demo.entities.User;
 import com.demoweb.demo.repositories.UserRepository;
+import com.demoweb.demo.services.exceptions.DataBaseException;
 import com.demoweb.demo.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +34,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-	    repository.deleteById(id);
+	    try {
+		repository.deleteById(id);
+	    }
+	    catch (EmptyResultDataAccessException e) {
+		throw new ResourceNotFoundException(id);
+	    }
+	    catch (DataIntegrityViolationException e) {
+		throw new DataBaseException(e.getMessage()); 
+	    }
 	}
 	
 	public User update(Long id, User obj) {
